5bb196c33848306db33b24e7bf2e2ac42ffbf11c
Olaf Alders <olaf@wundersolutions.com>
2018-12-17 22:25:29 -0500

    Fix errors in docs

--- lib/Git/Helpers.pm
+++ lib/Git/Helpers.pm
@@ -118,12 +118,22 @@ sub travis_url {
 
 =head1 SYNOPSIS
 
-    use Git::Helpers qw( checkout_root remote_url);
-    my $root = checkout_root();
+    use Git::Helpers qw(
+        checkout_root
+        current_branch_name
+        https_remote_Url
+        is_inside_work_tree
+        remote_url
+        travis_url
+    );
 
-    my $remote_url = remote_url('upstream');
+    my $dir              = '/path/to/folder/in/git/checkout';
+    my $root             = checkout_root($dir);
+    my $current_branch   = current_branch_name();
     my $https_remote_url = https_remote_url();
-    my $travis_url = travis_url();
+    my $inside_work_tree = is_inside_work_tree();
+    my $remote_url       = remote_url('upstream');
+    my $travis_url       = travis_url();
 
 =head2 checkout_root( $dir )
 
@@ -156,9 +166,9 @@ Defaults to master branch, but can also display current branch.
 
 =head2 is_inside_work_tree
 
-Returns C<true> if C<git rev-parse --is-inside-git-dir> returns C<true>.
+Returns C<true> if C<git rev-parse --is-inside-work-tree> returns C<true>.
 Otherwise returns C<false>. This differs slightly from the behaviour of
-C<--is-inside-git-dir> in real life, since it returns C<fatal> rather than
+C<--is-inside-work-tree> in real life, since it returns C<fatal> rather than
 C<false> if run outside of a git repository.
 
 =cut
